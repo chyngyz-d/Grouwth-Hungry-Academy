c6e14f39f618a504724769646681defd54088dcc
Chyngyz Duishenbaev <c.duishenbaev01@gmail.com>
2025-10-17 10:49:06 +0600

    test

--- Python/list1.py
+++ Python/list1.py
@@ -15,7 +15,7 @@
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
-  # +++your code here+++
+  # +++your code here++++
   return
 
 
